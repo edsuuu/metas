8e60499801f923d6cd73524b816d6606b207c5bb
edsuuu <edsonteles343@gmail.com>
2026-04-13 21:13:47 -0300

    first commit

--- app/Livewire/Support/TicketDetails.php
+++ app/Livewire/Support/TicketDetails.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Support;
 
 use App\Models\SupportTicket;
 use App\Models\SupportTicketReply;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
